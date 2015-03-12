1e4e9f750263c667a67b4b9d15c306f83ec64380
hmlingesh <Lingesh.M@happiestminds.com>
2015-03-12 13:34:00 +0530

    Update tomcat_instance.rb

--- lib/java_buildpack/container/tomcat/tomcat_instance.rb
+++ lib/java_buildpack/container/tomcat/tomcat_instance.rb
@@ -36,9 +36,9 @@ module JavaBuildpack
 
       # (see JavaBuildpack::Component::BaseComponent#compile)
       def compile
-        puts @application.root
+        #puts @application.root
         #FileUtils.mkdir_p @application.root + "jprofiler8"
-        RESOURCES_DIRECTORY = Pathname.new(File.expand_path('../../../../resources', __FILE__)).freeze
+        #RESOURCES_DIRECTORY = Pathname.new(File.expand_path('../../../../resources', __FILE__)).freeze
         #resources = RESOURCES_DIRECTORY + "jprofiler8"
         #FileUtils.cp_r("#{resources}/.", @application.root.to_s+ "jprofiler8")
         
