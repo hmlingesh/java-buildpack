d0541d25006683c23756c1bc19950e2245d923c0
hmlingesh <Lingesh.M@happiestminds.com>
2015-05-08 11:46:33 +0530

    Update dyna_trace_agent.rb

--- lib/java_buildpack/framework/dyna_trace_agent.rb
+++ lib/java_buildpack/framework/dyna_trace_agent.rb
@@ -52,7 +52,8 @@ module JavaBuildpack
 
       # (see JavaBuildpack::Component::VersionedDependencyComponent#supports?)
       def supports?
-        @application.services.one_service? FILTER, 'server'
+        true
+        #@application.services.one_service? FILTER, 'server'
       end
 
       private
